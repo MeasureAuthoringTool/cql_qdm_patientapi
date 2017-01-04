387ea77538182833283af65e6341e7a05192304c
holmesie <aholmes@mitre.org>
2017-01-04 16:21:37 -0500

    Adjusted context for params

--- vendor/assets/javascripts/cql4browsers.js
+++ vendor/assets/javascripts/cql4browsers.js
@@ -41929,7 +41929,7 @@
       this._parameters = _parameters != null ? _parameters : {};
       this.context_values = {};
       this.library_context = {};
-      if (Object.keys(this._parameters).length === 0 && this.parent.hasOwnProperty('_parameters')) {
+      if (this.parent != null && Object.keys(this._parameters).length === 0) {
         this._parameters = this.parent._parameters;
       }
     }
