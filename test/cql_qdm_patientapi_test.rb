8e127fa6561c1159b7163b3ca2db123cb0b42b1c
Samuel Sayer <ssayer@mitre.org>
2017-08-08 14:14:23 -0400

    remove extraneous files

--- test/cql_qdm_patientapi_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CqlQdmPatientapiTest < ActiveSupport::TestCase
-  test "truth" do
-    refute_nil CqlQdmPatientapi::VERSION
-    assert_kind_of Module, CqlQdmPatientapi
-  end
-end
