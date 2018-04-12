671de6a894731ae00266fbe9575fa8bd238ce476
Lizzie DeYoung <edeyoung@mitre.org>
2018-04-12 08:35:53 -0400

    updated to latest cql4browsers. includes all recent changes in the cql4browsers repo.

--- vendor/assets/javascripts/cql4browsers.js
+++ vendor/assets/javascripts/cql4browsers.js
@@ -493,13 +493,7 @@
   };
 
   codesMatch = function(code1, code2) {
-    if (code1.code !== code2.code) {
-      return false;
-    }
-    if (code1.system !== code2.system) {
-      return false;
-    }
-    return true;
+    return code1.code === code2.code && code1.system === code2.system;
   };
 
   module.exports.CodeSystem = CodeSystem = (function() {
@@ -3583,7 +3577,51 @@
 (function() {
   var aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration, element, expression, external, i, instance, interval, j, len, len1, lib, libs, list, literal, logical, nullological, overloaded, parameters, quantity, query, ref, reusable, string, structured, type;
 
-  expression = require('./expression', aggregate = require('./aggregate', arithmetic = require('./arithmetic', clinical = require('./clinical', comparison = require('./comparison', conditional = require('./conditional', datetime = require('./datetime', declaration = require('./declaration', external = require('./external', instance = require('./instance', interval = require('./interval', list = require('./list', literal = require('./literal', logical = require('./logical', nullological = require('./nullological', parameters = require('./parameters', quantity = require('./quantity', query = require('./query', reusable = require('./reusable', string = require('./string', structured = require('./structured', type = require('./type', overloaded = require('./overloaded')))))))))))))))))))))));
+  expression = require('./expression');
+
+  aggregate = require('./aggregate');
+
+  arithmetic = require('./arithmetic');
+
+  clinical = require('./clinical');
+
+  comparison = require('./comparison');
+
+  conditional = require('./conditional');
+
+  datetime = require('./datetime');
+
+  declaration = require('./declaration');
+
+  external = require('./external');
+
+  instance = require('./instance');
+
+  interval = require('./interval');
+
+  list = require('./list');
+
+  literal = require('./literal');
+
+  logical = require('./logical');
+
+  nullological = require('./nullological');
+
+  parameters = require('./parameters');
+
+  quantity = require('./quantity');
+
+  query = require('./query');
+
+  reusable = require('./reusable');
+
+  string = require('./string');
+
+  structured = require('./structured');
+
+  type = require('./type');
+
+  overloaded = require('./overloaded');
 
   libs = [expression, aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration, external, instance, interval, list, literal, logical, nullological, parameters, query, quantity, reusable, string, structured, type, overloaded];
 
